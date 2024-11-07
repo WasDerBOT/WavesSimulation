9f0ad06ba4a460e09d121203a054c092940ee341
artem <artem.edokov@mail.ru>
2024-11-08 00:36:38 +0300

    Переделал структуру окон, добавил шаблоны.

--- main.py
+++ main.py
@@ -1,42 +1,71 @@
 import sys
 
-from PyQt6.QtCore import QThread
-from PyQt6.QtCore import QTimer
+from PyQt6.QtCore import QTimer, QStringListModel
 from PyQt6.QtGui import QPainter
-from PyQt6.QtWidgets import QApplication
+from PyQt6.QtWidgets import QApplication, QListView
 from PyQt6.QtWidgets import QWidget, QMainWindow
-from PyQt6.uic import loadUi
-
+from templates.main_window import Ui_MainWindow
+from templates.entry import Ui_Greeting
+from templates.create import Ui_Create
+from templates.load import Ui_Load
+from templates.save import Ui_Save
+from templates.menu import Ui_Menu
 from Physic_classes import Plane
 
 
-class Main(QMainWindow):
+class Main(QMainWindow, Ui_MainWindow):
     def __init__(self, plane: Plane):
         super().__init__()
-        loadUi("main.ui", self)
+        self.setupUi(self)
         plane.painter = QPainter(self)
         self.timer = QTimer()
         self.timer.timeout.connect(plane.process)
-        self.timer.start(30)
+        # self.timer.start(30)
         self.painter = QPainter(self)
 
-    def update_screen(self):
-        pass
 
-
-
-
-
-class Entry(QWidget):
+class Entry(QWidget, Ui_Greeting):
     def __init__(self):
         super().__init__()
-        loadUi("entry.ui", self)
+        self.setupUi(self)
 
 
-class Create(QWidget):
+class Create(QWidget, Ui_Create):
     def __init__(self):
         super().__init__()
-        loadUi("create.ui", self)
+        self.setupUi(self)
+
+
+class Load(QWidget, Ui_Load):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.listView = self.findChild(QListView, "listView")
+        self.model = QStringListModel()
+        self.listView.setModel(self.model)
+        self.model.insertRow(0)
+        self.model.setData(self.model.index(0, 0), "First save example")
+        self.model.insertRow(1)
+        self.model.setData(self.model.index(1, 0), "Second save example")
+
+
+class Save(QWidget, Ui_Save):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.listView = self.findChild(QListView, "listView")
+        self.model = QStringListModel()
+        self.listView.setModel(self.model)
+        self.model.insertRow(0)
+        self.model.setData(self.model.index(0, 0), "First save example")
+        self.model.insertRow(1)
+        self.model.setData(self.model.index(1, 0), "Second save example")
+
+
+class Menu(QWidget, Ui_Menu):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
 
 
 FPS = 30
@@ -48,7 +77,13 @@ Field = Plane(550, 650)
 window = Main(Field)
 entry = Entry()
 create = Create()
-
+load = Load()
+save = Save()
+load.show()
+save.show()
+create.show()
+entry.show()
 window.show()
-
+menu = Menu()
+menu.show()
 sys.exit(app.exec())
